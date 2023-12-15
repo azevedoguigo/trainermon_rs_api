27c8baafc2b73a416df03630bdad231b98e50e7f
Guilherme Azevedo <guiazevedo.deb@gmail.com>
2023-12-14 21:21:26 -0300

    feat: create trainer.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
-mod user;
+mod trainer;
 
 use actix_web::{HttpResponse, HttpServer, App, post, web};
 use mongodb::Client;
-use crate::user::User;
+use crate::trainer::Trainer;
 
 const DB_NAME: &str = "trainermon";
-const COLL_NAME: &str = "users";
+const COLL_NAME: &str = "trainers";
 
-#[post("/add_user")]
-async fn add_user(client: web::Data<Client>, form: web::Form<User>) -> HttpResponse {
+#[post("/add_trainer")]
+async fn add_user(client: web::Data<Client>, form: web::Form<Trainer>) -> HttpResponse {
     let collection = client.database(DB_NAME).collection(COLL_NAME);
     let result = collection.insert_one(form.into_inner(), None).await;
     
     match result {
-        Ok(_) => HttpResponse::Ok().body("User added!"),
+        Ok(_) => HttpResponse::Ok().body("Trainer added!"),
         Err(err) => HttpResponse::InternalServerError().body(err.to_string())
     }
 }
--- src/trainer.rs
+++ src/trainer.rs
@@ -1,8 +1,8 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, PartialEq, Eq, Deserialize, Serialize)]
-pub struct User {
-  	pub name: String,
+pub struct Trainer {
+    pub name: String,
     pub email: String,
     pub password: String
 }

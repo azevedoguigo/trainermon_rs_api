924f4a1dd47a3a3321df757929e33fa8097b6813
Guilherme Azevedo <guiazevedo.deb@gmail.com>
2023-12-17 23:56:46 -0300

    refactor: add nickname field in trainer model.

--- src/trainer.rs
+++ src/trainer.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, PartialEq, Eq, Deserialize, Serialize)]
 pub struct Trainer {
+    pub nickname: String,
     pub name: String,
     pub email: String,
     pub password: String
